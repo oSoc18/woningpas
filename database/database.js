71780b88fc2d3053dbb0e701e18808a2fba176c2
RaisedDjed <pierre-alexandre.blanc@ulb.ac.be>
2018-07-09 10:08:54 +0200

    removing database

--- database/database.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-console.log("test");
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-}).listen(8000)
