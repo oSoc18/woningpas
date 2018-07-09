04e7caa2930b1404ec08a0009f695f7e177a2605
GracienLofa <lofgr@ymail.com>
2018-07-09 12:14:12 +0200

    Created 3 components

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">
+        <img id="logo" src=""/>
+      </b-navbar-brand>
+      <b>Vlaanderen</b>
+      <b> Dit is een officiële website van de Vlaamse overheid</b>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#" disabled>Vlaanderen</b-nav-item>
+          <b-nav-item href="#" disabled>WONINGPAS</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right>
+            <em class="name">{{ name }</em>
+            <img id="picture" src=""/>
+            <a href="#">CONTACTEER ONS</a>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="sidebar">
+    <img id="houseImage" />
+    <h2>{{ Address }}</h2>
+    <h3>{{ City }}</h3>
+    <hr><br>
+    <ul>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          <img id="houseIcon"/>&nbsp;&nbsp;
+          Mijn woning
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Gebouw, indeling, ligging
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Energie
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          isolatie & beglazing
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          <img id="documentIcon"/>&nbsp;&nbsp;
+          Attesten & documenten
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
