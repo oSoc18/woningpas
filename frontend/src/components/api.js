81aa62558e0a98c18c04ebacefc3d80e4c2d9903
Axtux <axtux@hotmail.com>
2018-07-18 12:02:23 +0200

    clean

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,28 +1,28 @@
 import axios from 'axios'
 
-const api = {
-  request: function(path, data, success, error){
-    axios({
-      url: 'http://localhost:8080/' + path,
-      data: data,
-      method: 'POST',
-      responseType: 'json',
-    }).then(response => {
-      let data = response.data;
-      if(success) {
-        success(data);
-      } else {
-        alert(JSON.stringify(data));
-      }
-    }).catch(err => {
-      let message = err.response.data.message;
-      if(error) {
-        error(message);
-      } else {
-        alert('ERROR: ' + message);
-      }
-    });
-  }
+function request(path, data, success, error){
+  axios({
+    url: 'http://localhost:8080/' + path,
+    data: data,
+    method: 'POST',
+    responseType: 'json',
+  }).then(response => {
+    let data = response.data;
+    if(success) {
+      success(data);
+    } else {
+      alert(JSON.stringify(data));
+    }
+  }).catch(err => {
+    let message = err.response.data.message;
+    if(error) {
+      error(message);
+    } else {
+      alert('ERROR: ' + message);
+    }
+  });
 }
 
-export default api
+export default {
+  request: request
+}
