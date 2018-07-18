c6282ff0999118efebf0ded5da8058edf3370164
RaisedDjed <pierre-alexandre.blanc@ulb.ac.be>
2018-07-18 14:11:57 +0200

    database separated

---
+++ backend/database.js
@@ -0,0 +1,38 @@
+async function populateDB() {
+    let account1 = await smartcontract.createAccount();
+    let account2 = await smartcontract.createAccount();
+    let accountInspector = await smartcontract.createAccount();
+    var pop = {
+        "owner1@woningpas.be": {
+            type: "owner",
+            houses: {
+                house1_id: {
+                    
+                    certificate1: true,
+                    certificate2: true
+                },
+                house2_id: {
+                    certificate3: true
+                }
+            },
+            ethereum: account1
+        },
+        "owner2@woningpas.be": {
+            type: "owner",
+            houses: {
+                house1_id: {
+                    certificate1: true,
+                    certificate2: true
+                }
+            },
+            ethereum: account2
+        },
+        "inspector1@woningpas.be": {
+            type: "inspector",
+            ethereum: accountInspector
+        }
+    }
+    fs.writeFile("./database.json", JSON.stringify(pop))
+}
+
+populateDB()
--- backend/server.js
+++ backend/server.js
@@ -253,45 +253,6 @@ app.get('/listFiles', function (req, res) {
 })
 //*/
 
-async function populateDB() {
-    let account1 = await smartcontract.createAccount();
-    let account2 = await smartcontract.createAccount();
-    let accountInspector = await smartcontract.createAccount();
-    var pop = {
-        "owner1@woningpas.be": {
-            type: "owner",
-            houses: {
-                house1_id: {
-                    
-                    certificate1: true,
-                    certificate2: true
-                },
-                house2_id: {
-                    certificate3: true
-                }
-            },
-            ethereum: account1
-        },
-        "owner2@woningpas.be": {
-            type: "owner",
-            houses: {
-                house1_id: {
-                    certificate1: true,
-                    certificate2: true
-                }
-            },
-            ethereum: account2
-        },
-        "inspector1@woningpas.be": {
-            type: "inspector",
-            ethereum: accountInspector
-        }
-    }
-    fs.writeFile("./database.json", JSON.stringify(pop))
-}
-
-//populateDB()
-
 var server = app.listen(8080, function() {
     var host = server.address().address
     var port = server.address().port
